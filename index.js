69aa0e0967000e0568d0e1679179dcd4d411faef
dsatpm <jeremiahjh@gmail.com>
2023-10-01 12:09:56 -0700

    added customizable color options, fixed shapes.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 // Imported programs required for this application
 const fs = require('fs-extra');
 const inquirer = require('inquirer');
@@ -23,30 +22,80 @@ async function generateLogo() {
 				choices: ['Triangle', 'Circle', 'Square'],
 			},
 
-      {
-        type: 'list',
-        name: 'colorChoice',
-        message: 'Pick the color you want the shape to be',
-        choices: [ 
-				'Black',
-				'Dark Red',
-				'Navy',
-				'Olive',
-				'Indigo',
-				'Saddle Brown',
-				'Hot Pink',
-			],
+			{
+				type: 'list',
+				name: 'textColor',
+				message: 'Choose your text color',
+				choices: ['Black', 'White', 'Royal Blue', 'Tomato', 'Custom'],
+
 				// Converts choice to lower case and removes spaces to plug into image generator
-				filter: (choice) => choice.toLowerCase().replace(/ /g, ''),
-      },
+				filter: (choice) =>
+					choice.toLowerCase().replace(/ /g, '') === 'custom'
+						? 'custom'
+						: choice.toLowerCase().replace(/ /g, ''),
+			},
+
+			// Allows a user to enter a custom color
+			{
+				type: 'input',
+				name: 'customTextColor',
+				message:
+					'Enter your custom text color (can be hexadecimal(#000808) or color name)',
+				filter: (choice) =>
+					choice.toLowerCase().replace(/ /g, ''),
+				when: (answers) => answers.textColor === 'custom',
+			},
+
+			{
+				type: 'list',
+				name: 'colorChoice',
+				message: 'Pick the color you want the shape to be',
+				choices: ['Black', 'Dark Red', 'Olive', 'Hot Pink', 'Custom'],
+
+				// Converts choice to lower case and removes spaces to plug into image generator
+				filter: (choice) =>
+					choice.toLowerCase().replace(/ /g, '') === 'custom'
+						? 'custom'
+						: choice.toLowerCase().replace(/ /g, ''),
+			},
+
+			// Allows user to enter a custom color
+			{
+				type: 'input',
+				name: 'customShapeColor',
+				message:
+					'Enter your custom shape color (can be hexadecimal (#181818) or color name)',
+				filter: (choice) =>
+					choice.toLowerCase().replace(/ /g, ''),
+				when: (answers) => answers.colorChoice === 'custom',
+			},
 		]);
 
-		// Variable declaration that holds user input and pulls info from 'svg-builder.js'
-		const svgCreator = buildSVG(logoParams);
+		// Creates object variable that store all user parameters into 'logoParams'
+		const {
+			logo,
+			shape,
+			textColor,
+			customTextColor,
+			colorChoice,
+			customShapeColor,
+		} = logoParams;
+
+		// Variables that change depending on user selection
+		const textColorChoice = customTextColor || textColor;
+		const shapeColorChoice = customShapeColor || colorChoice;
+
+		// Variable declaration that user pulls info from 'svg-builder.js'
+		const svgCreator = buildSVG({
+			logo,
+			shape,
+			textColor: textColorChoice,
+			colorChoice: shapeColorChoice,
+		});
 
 		// Writes a file to 'examples' directory and names the file 'logo.svg'
 		await fs.writeFile('./examples/logo.svg', svgCreator);
-		console.log('logo.svg successfully generated!');
+		console.log('Generated logo.svg');
 	} catch (error) {
 		console.error('An error occurred:', error);
 	}
--- public/scripts/svg-builder.js
+++ public/scripts/svg-builder.js
@@ -1,56 +1,29 @@
 const { Triangle, Square, Circle } = require('../../lib/shapes.js');
 
 function buildSVG(userParams) {
-  const { logo, shape, colorChoice } = userParams;
+  const { logo, shape, textColor, colorChoice } = userParams;
 
   let shapeStyle = '';
   let textAlign = '';
   let viewBox = '';
   let aspectRatio = 'xMidYMid meet';
-  let textColor = 'white';
-
-  switch (colorChoice) {
-    case 'black':
-      textColor = '#f5f5f5';
-      break;
-    case 'darkred':
-      textColor = '#dc806f';
-      break;
-    case 'navy':
-      textColor = '#daa520';
-      break;
-    case 'olive':
-      textColor = '#c71585';
-      break;
-    case 'indigo':
-      textColor = '#fdb515';
-      break;
-    case 'saddlebrown':
-      textColor = '#fffdd0';
-      break;
-    case 'hotpink':
-      textColor = '#00ced1';
-      break;
-    default:
-      textColor = 'white';
-      break;
-  }
+ 
   switch (shape) {
     case 'Triangle':
-      const triangle = new Triangle(colorChoice, 300, 200);
-        shapeStyle = triangle.getSvgParams(textColor);
+      const triangle = new Triangle(colorChoice, textColor, 300, 200);
+        shapeStyle = triangle.getSvgParams();
       viewBox = '0 0 300 200';
       textAlign = `<text x="50%" y="35%" text-anchor="middle" dominant-baseline="middle" font-size="60" font-weight="bold" fill="${textColor}">${logo}</text>`;
       break;
     case 'Circle':
-      const circle = new Circle(colorChoice, 200);
-        shapeStyle = circle.getSvgParams(textColor);
+      const circle = new Circle(colorChoice, textColor, 200);
+        shapeStyle = circle.getSvgParams();
       viewBox = '0 0 400 400';
       textAlign = `<text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle" font-size="140" font-weight="bold" fill="${textColor}">${logo}</text>`;
       break;
     case 'Square':
-      const square = new Square(colorChoice, 300);
-        shapeStyle = square.getSvgParams(textColor);
+      const square = new Square(colorChoice, textColor, 300);
+        shapeStyle = square.getSvgParams();
       viewBox = '0 0 300 300';
       textAlign = `<text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle" font-size="104" font-weight="bold" fill="${textColor}">${logo}</text>`;
       break;
